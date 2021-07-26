f4a0bd9a9fd852c6ae02121ec71dba9cd3af413b
Teofilo Acholla Ligawa Gafna <teofilo48ligawa@gmail.com>
2021-07-26 09:05:05 +0300

    installed from swirl - getting and cleaning data

---
+++ Getting and cleaning data, 1st swirl session.R
@@ -0,0 +1,13 @@
+library(swirl)
+package_version(swirl)
+package_version(swirl()
+                
+Teofilo Acholla Ligawa Gafna
+1
+0
+2
+install_from_swirl("Getting and Cleaning Data")
+swirl()
+Teofilo Acholla Ligawa Gafna
+1
+1
